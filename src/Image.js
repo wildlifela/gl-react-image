d01727e52a87b41c2fca68052cc0a7777c9e080d
Christopher Dro <hello@blick-labs.com>
2017-10-24 17:23:12 -0700

    Use prop-types package

--- src/Image.js
+++ src/Image.js
@@ -1,5 +1,6 @@
 import GL from "gl-react";
-import React, {PropTypes} from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import rectCrop from "rect-crop";
 import rectClamp from "rect-clamp";
 
